44f602e795869c7eed00e9294a24231d8f545fee
kk3747 <3747krishna@gmail.com>
2024-07-06 23:13:26 +0530

    Initial commit

---
+++ dbConnect.php
@@ -0,0 +1,17 @@
+<?php
+    $severname = "mysqldb";
+    $username = "root";
+    $password = "";
+    $database = "faculty";
+
+    $conn = mysqli_connect($severname , $username , $password , $database ) ;
+
+    if( !$conn ) {
+        die("Failed to Connect : " . mysqli_connect_error());
+    }
+    else {
+        echo "Connection was successfull<br>";
+    }
+
+
+?>
